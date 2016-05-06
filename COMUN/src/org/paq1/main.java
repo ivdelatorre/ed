2bac6de94bc4e127ad11949e9fd4088b661a6e6e
IvandelaTorre <ivandelatorre.daw@gmail.com>
2016-05-06 10:39:47 +0200

    Modificaciones en Main

--- COMUN/src/org/paq1/main.java
+++ COMUN/src/org/paq1/main.java
@@ -1,7 +1,8 @@
 package org.paq1;
 
 public class main {
-public static void main(String[] args) {
-	System.out.println("Hola java. Soy Harry");
-}
+	public static void main(String[] args) {
+		System.out.println("Hola java. Soy Harry");
+		System.out.println("nuevo");
+	}
 }
